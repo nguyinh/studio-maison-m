f2c7ca5fd635ea640a4f54f23be334b87b6d7fa5
nguyinh <florent.dupontnguyen@gmail.com>
2024-04-02 20:03:21 +0200

    Fix character

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="font-chango">
-      <span>Hello Samsam ! Y'a pas grand chose pour le moment</span>
+      <span>Hello Samsam ! Y&rsquo;a pas grand chose pour le moment</span>
     </main>
   );
 }
